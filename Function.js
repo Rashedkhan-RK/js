b9e04e76ea292534c2d8724361f1fb7153677a92
Md Rashed Khan Milon <98528165+Rashedkhan-RK@users.noreply.github.com>
2022-04-10 11:35:09 +0600

    first commit

---
+++ Function.js
@@ -0,0 +1,5 @@
+function showMyName(){
+    console.log('my name is Khan')
+    console.log('my name is milon')
+}
+showMyName();
---
+++ script.js
@@ -0,0 +1,16 @@
+var begunprice = 100;
+if(begunprice <=80){
+console.log('begunir chop khabo')
+}else{
+    console.log("misti kumrar chop khao")
+}
+var friend = ['Raihan','Hridoy','Sagar','Ismail'];
+console.log(friend);
+var i = 0;
+while(i<=10){
+    console.log(i)
+    i++
+}
+for(i=0; i<=10; i++){
+    console.log(i)
+}
