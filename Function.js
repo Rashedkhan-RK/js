acd81fdff21fbe2d41b2464dd91313717975a703
Md Rashed Khan Milon <98528165+Rashedkhan-RK@users.noreply.github.com>
2022-04-10 12:43:09 +0600

    yygug

--- Function.js
+++ Function.js
@@ -2,4 +2,12 @@ function showMyName(){
     console.log('my name is Khan')
     console.log('my name is milon')
 }
-showMyName();
+showMyName(); 
+function Lipyear(year){
+    if(year%4==0){
+        console.log('This is a lipyear');
+    }else{
+        console.log('This is not lipyear');
+    }
+}
+Lipyear(2020)
