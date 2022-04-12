8b6554d908329b2d1beb75c02bb5a7e1a971ecf0
Md Rashed Khan Milon <98528165+Rashedkhan-RK@users.noreply.github.com>
2022-04-12 12:18:50 +0600

    class_4

--- Function.js
+++ Function.js
@@ -1,13 +1,27 @@
-function showMyName(){
-    console.log('my name is Khan')
-    console.log('my name is milon')
-}
-showMyName(); 
-function Lipyear(year){
-    if(year%4==0){
-        console.log('This is a lipyear');
+// function showMyName(){
+//     console.log('my name is Khan')
+//     console.log('my name is milon')
+// }
+// showMyName(); 
+// function Lipyear(year){
+//     if(year%4==0){
+//         console.log('This is a lipyear');
+//     }else{
+//         console.log('This is not lipyear');
+//     }
+// }
+// Lipyear(2024);
+// this is arrow Function
+// let sum = (number1, number2) => number1 + number2
+// console.log(sum(25, 10))
+ 
+function num(x,y,z){
+    if(x>y && X>Z){
+        console.log(x + 'big number')
+    }else if(y>x && y>z){
+        console.log(y + 'big numbr')
     }else{
-        console.log('This is not lipyear');
+        console.log(x + 'big number')
     }
 }
-Lipyear(2020)
+num(3, 10, 7)
--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-var begunprice = 100;
-if(begunprice <=80){
-console.log('begunir chop khabo')
-}else{
-    console.log("misti kumrar chop khao")
-}
-var friend = ['Raihan','Hridoy','Sagar','Ismail'];
-console.log(friend);
-var i = 0;
-while(i<=10){
-    console.log(i)
-    i++
-}
-for(i=0; i<=10; i++){
-    console.log(i)
-}
+// var begunprice = 100;
+// if(begunprice <=80){
+// console.log('begunir chop khabo')
+// }else{
+//     console.log("misti kumrar chop khao")
+// }
+// var friend = ['Raihan','Hridoy','Sagar','Ismail'];
+// console.log(friend);
+// var i = 0;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+// for(i=0; i<=10; i++){
+//     console.log(i)
+// }
+// let rain = false;
+// let dal = true;
+// if(rain==true || dal==true){
+//     document.write('khichuri ranna hobe')
+// }else{
+//     document.write('khichuri ranna hobe na')
+// }
+let h1tag = document.getElementsByTagName('h1');
+h1tag[1].innerText='Rashed Khan Milon'
+h1tag[0].style.color='red'
